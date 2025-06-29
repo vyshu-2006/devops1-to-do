9cac6b62aff58317e311b82a50e69c3a6778eecf
Vaishnavi Moturi <moturi.vaishnavi2006@gmail.com>
2025-06-29 19:00:07 +0530

    Add files via upload

--- app/todo/app.py
+++ app/todo/app.py
@@ -1,1 +1,25 @@
 
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
